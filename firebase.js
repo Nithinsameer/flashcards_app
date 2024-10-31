85aba99454fd2d760078ba652047db4428a57653
Nithinsameer <73516441+Nithinsameer@users.noreply.github.com>
2024-10-31 18:12:51 -0400

    updated firebase code

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,18 +8,26 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCNapVW5LN3lLnGg3RptYsHniB-9D3LJFg",
-  authDomain: "flashcards-95e90.firebaseapp.com",
-  projectId: "flashcards-95e90",
-  storageBucket: "flashcards-95e90.appspot.com",
-  messagingSenderId: "1013375808577",
-  appId: "1:1013375808577:web:3114951477f30d0585c2c0",
-  measurementId: "G-QCYZN9CCYZ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+if (typeof window !== 'undefined') {
+    isSupported().then((supported) => {
+        if (supported) {
+            const analytics = getAnalytics(app);
+        }
+    });
+}
+
 const db = getFirestore(app)
 
 export {db}
